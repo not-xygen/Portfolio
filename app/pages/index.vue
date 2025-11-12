4452bac78b428ab40359135e5d9ebce23b149065
Dhaffa Agus <11201020@student.itk.ac.id>
2025-11-13 00:05:35 +0800

    feat: wrap content in ClientOnly and BlurReveal components for improved rendering

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,8 +11,7 @@
           <Tetris 
             class="h-[500px] w-full mask-[radial-gradient(450px_circle_at_center,#4CC9FE,transparent)]" 
             :base="15"
-            square-color="#4CC9FE" 
-          />
+            square-color="#4CC9FE" />
         </ClientOnly>
       </div>
       <div class="flex flex-col md:flex-row justify-around items-center pt-6 md:pt-12 gap-4 lg:gap-0">
@@ -35,17 +34,31 @@
     <!-- Short Introduction -->
     <AppContentSide />
     <AppContentMain class="p-8 lg:p-24 bg-stone-50 dark:bg-stone-950 border-t">
-      <p >Lulusan Teknik Informatika dari Institut Teknologi Kalimantan (IPK 3,26/4,0) dengan pengalaman mengembangkan proyek web dan perangkat lunak nyata. Berfokus pada pembuatan solusi digital yang efisien dan berorientasi pada pengguna, serta terus mengasah kemampuan di bidang pengembangan frontend dan backend. Memiliki ketertarikan pada desain sistem, arsitektur bersih, dan penerapan teknologi yang selaras dengan kebutuhan bisnis. Termotivasi untuk membangun alat yang menyederhanakan alur kerja dan membuat teknologi lebih mudah diakses oleh pengguna.</p>
+      <ClientOnly>
+        <BlurReveal :delay="0.2" :duration="0.75" class="">
+          <p>Lulusan Teknik Informatika dari Institut Teknologi Kalimantan (IPK 3,26/4,0) dengan pengalaman
+            mengembangkan proyek web dan perangkat lunak nyata. Berfokus pada pembuatan solusi digital yang efisien dan
+            berorientasi pada pengguna, serta terus mengasah kemampuan di bidang pengembangan frontend dan backend.
+            Memiliki ketertarikan pada desain sistem, arsitektur bersih, dan penerapan teknologi yang selaras dengan
+            kebutuhan bisnis. Termotivasi untuk membangun alat yang menyederhanakan alur kerja dan membuat teknologi
+            lebih mudah diakses oleh pengguna.</p>
+        </BlurReveal>
+      </ClientOnly>
     </AppContentMain>
     <AppContentSide />
 
     <!-- Contact -->
     <AppContentSide />
     <AppContentMain class="p-8 lg:p-24 bg-stone-50 dark:bg-stone-950 border-t">
-      <h1 class="text-4xl font-bold mb-8">Website sedang dibangun</h1>
-      <div class="prose dark:prose-invert">
-        <p>Halaman ini sedang dibangun. Konten akan segera mencakup portofolio proyek, studi kasus teknis, daftar keterampilan, dan informasi kontak profesional.</p>
-      </div>
+      <ClientOnly>
+        <BlurReveal :delay="0.2" :duration="0.75" class="">
+          <h1 class="text-4xl font-bold mb-8">Website sedang dibangun</h1>
+          <div class="prose dark:prose-invert">
+            <p>Halaman ini sedang dibangun. Konten akan segera mencakup portofolio proyek, studi kasus teknis, daftar
+              keterampilan, dan informasi kontak profesional.</p>
+          </div>
+        </BlurReveal>
+      </ClientOnly>
     </AppContentMain>
     <AppContentSide />
   </AppContainer>
